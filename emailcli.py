d20645a9e731d8e8e9151e176b49e5837c3d0cb7
Liam Lin <57537887+liamlincs@users.noreply.github.com>
2023-06-02 16:40:04 +0800

    bug fix
    修复bug

--- emailcli.py
+++ emailcli.py
@@ -1,18 +1,23 @@
 import os
 import configparser
-import logging
 import argparse
 import poplib
 import threading
 import traceback
-from imapclient import IMAPClient
-from email import message_from_bytes
-
+import schedule
+import time
 import socket
 import socks
+import base64
+import logging
+import datetime
+import ssl
+from logging.handlers import RotatingFileHandler
+from imapclient import IMAPClient
+from email import message_from_bytes
+from cryptography.fernet import Fernet
 
 DEFAULT_TIMEOUT = 30
-
 # Configuration file
 CONFIG_FILE = 'config.ini'
 
@@ -21,9 +26,12 @@ config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
 
 # Get configuration parameters
+schedule_minutes = int(config.get('settings', 'schedule_minutes'))
 max_emails = int(config.get('settings', 'max_emails'))
 email_output_dir = config.get('settings', 'email_output_dir')
+email_export_dir = config.get('settings', 'email_export_dir')
 proxy = config.get('settings', 'proxy')
+debugFlag = config.getboolean('settings', 'debug')
 
 # Create a command line argument parser
 parser = argparse.ArgumentParser(description="Email client for POP3 and IMAP4")
@@ -36,10 +44,27 @@ parser.add_argument('--protocol', help='Email protocol (POP3 or IMAP4)')
 args = parser.parse_args()
 
 # Configure logs
-logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s',
-                        handlers=[logging.StreamHandler(),
-                                  logging.FileHandler('emailcli.log', encoding='utf-8')])
+logger = logging.getLogger('emailcli_logger')
+logger.setLevel(logging.DEBUG)
+
+os.makedirs("logs", exist_ok=True)
+info_handler = RotatingFileHandler('logs/info_emailcli.log', maxBytes=1024*1024*10, backupCount=10)
+info_handler.setLevel(logging.INFO)
+
+debug_handler = RotatingFileHandler('logs/debug_emailcli.log', maxBytes=1024*1024*10, backupCount=50)
+debug_handler.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s (line: %(lineno)d)')
+info_handler.setFormatter(formatter)
+debug_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(info_handler)
+logger.addHandler(debug_handler)
+logger.addHandler(stream_handler)
 
 class EmailClient:
     def __init__(self, server, username, password, protocol='POP3', proxy=None):
@@ -50,31 +75,42 @@ class EmailClient:
         if proxy:
             proxy_protocol, proxy_host, proxy_port = proxy.split(':')
             if proxy_protocol.find('sock') != -1:
-                proxy_type = socks.SOCKS5
+                proxy_type = socks.PROXY_TYPE_SOCKS5
             else:
-                proxy_type = socks.HTTP
+                proxy_type = socks.PROXY_TYPE_HTTP
             proxy_host = proxy_host.replace('//', '')
+            
+            # socks.setdefaultproxy(proxy_type, proxy_host, int(proxy_port))
             socks.set_default_proxy(proxy_type, proxy_host, int(proxy_port))
-            socket.socket = socks.socksocket
+            # socks.wrap_module(poplib)
+            # socket.socket = socks.socksocket
 
     def _create_pop3_connection(self):
-        if self.protocol.find('s') != -1:
+        if self.protocol.find('S') != -1:
             if self.protocol.find(':') != -1:
                 port = int(self.protocol.split(':')[1])
             else:
                 port = 995
-            pop3_server = poplib.POP3_SSL(self.server, port, timeout=DEFAULT_TIMEOUT)
+
+            ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+            ctx.set_ciphers('DEFAULT:@SECLEVEL=1')
+            pop3_server = poplib.POP3_SSL(self.server, port, context=ctx, timeout=DEFAULT_TIMEOUT)
+            # sock = socks.socksocket()
+            # sock.connect(self.server, port)
+            # ssl_sock = ssl.wrap_socket(sock)
+            # pop3_server = poplib.POP3_SSL(ssl_sock)
         else:
             if self.protocol.find(':') != -1:
                 port = int(self.protocol.split(':')[1])
             else:
                 port = 110
+            
             pop3_server = poplib.POP3(self.server, timeout=DEFAULT_TIMEOUT)
 
         return pop3_server
 
     def _create_imap4_connection(self):
-        if self.protocol.find('s') != -1:
+        if self.protocol.find('S') != -1:
             if self.protocol.find(':') != -1:
                 port = int(self.protocol.split(':')[1])
             else:
@@ -86,7 +122,9 @@ class EmailClient:
             else:
                 port = 143
             bSSL = False
-        imap4_server = IMAPClient(self.server, port, ssl = bSSL, timeout=DEFAULT_TIMEOUT)
+        ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        ctx.set_ciphers('ALL')
+        imap4_server = IMAPClient(self.server, port, ssl = bSSL, ssl_context=ctx, timeout=DEFAULT_TIMEOUT)
         
         return imap4_server
 
@@ -104,7 +142,7 @@ class EmailClient:
             else:
                 raise ValueError("Invalid protocol. Supported protocols are 'POP3' and 'IMAP4'.")
         except:
-            logging.error(traceback.format_exc())
+            logger.info(f"[{self.username}] {traceback.format_exc()}")
             raise
 
     def fetch_emails(self):
@@ -115,52 +153,75 @@ class EmailClient:
 
     def _fetch_pop3_emails(self):
         try:
+            os.makedirs(os.path.join(email_output_dir, f"{datetime.datetime.now().strftime('%Y%m%d')}", self.username), exist_ok=True)
+
             downloaded_cnt = 0
+            neterr_cnt = 0
             email_count, _ = self.mailbox.stat()
+            logger.info(f"[{self.username}] has {email_count} messages in INBOX")
 
             # Read the acquired Message-ID from the file
-            mid_downloaded_path = os.path.join(email_output_dir, self.protocol, self.username)
+            mid_downloaded_path = os.path.join(email_output_dir, 'cache', self.username)
             mid_downloaded_file = os.path.join(mid_downloaded_path, "INBOX_mid_downloaded.txt")
             if os.path.exists(mid_downloaded_file):
                 with open(mid_downloaded_file, 'r') as f:
                     fetched_ids = f.read().splitlines()
             else:
                 fetched_ids = []
+                os.makedirs(os.path.dirname(mid_downloaded_file), exist_ok=True)
 
-            for email_id in range(1, email_count + 1):
-                response, lines, _ = self.mailbox.retr(email_id)
-                raw_msg = b'\r\n'.join(lines)
-                msg = message_from_bytes(raw_msg)
-                message_id = msg['Message-ID']
+            for email_id in range(email_count, 0, -1):
+                try:
+                    response, lines, _ = self.mailbox.retr(email_id)
+                    raw_msg = b'\r\n'.join(lines)
+                    msg = message_from_bytes(raw_msg)
+                    message_id = msg['Message-ID']
+                    if message_id is None:
+                        message_id = self.mailbox.uidl(email_id).decode().split(' ')[2]
 
-                # Check if the Message-ID already exists in the retrieved emails
-                if message_id not in fetched_ids:
+                    # Check if the Message-ID already exists in the retrieved emails
+                    if message_id not in fetched_ids:
 
-                    # Create a file name for the email
-                    safe_message_id = message_id.replace('<', '').replace('>', '')
+                        # Create a file name for the email
+                        
+                        safe_message_id = message_id.replace('<', '').replace('>', '')
 
-                    email_file = os.path.join(email_output_dir, self.protocol, self.username, "INBOX", f"{safe_message_id}.eml")
-                    os.makedirs(os.path.dirname(email_file), exist_ok=True)
-                    with open(email_file, 'wb') as f:
-                        f.write(raw_msg)
-                    logging.info(f"Saved to {safe_message_id}")
+                        email_file = os.path.join(email_output_dir, f"{datetime.datetime.now().strftime('%Y%m%d')}", self.username, "INBOX", f"{safe_message_id}.eml")
+                        os.makedirs(os.path.dirname(email_file), exist_ok=True)
+                        with open(email_file, 'wb') as f:
+                            f.write(raw_msg)
+                        logger.info(f"Saved to f'{email_file}'")
 
-                    # Add the new Message-ID to the list of already obtained Message-IDs and update the file
-                    fetched_ids.append(message_id)
-                    with open(mid_downloaded_file, 'a') as f:
-                        f.write(f"{message_id}\n")
-                    downloaded_cnt += 1
-                    if downloaded_cnt >= max_emails:
-                        logging.info(f"Reach maximum download limit {downloaded_cnt}")
+                        if email_export_dir != '':
+                            export_email_file = os.path.join(email_export_dir, self.username, "INBOX", f"{safe_message_id}.eml")
+                            os.makedirs(os.path.dirname(export_email_file), exist_ok=True)
+                            with open(export_email_file, 'wb') as f:
+                                f.write(raw_msg)
+                            logger.info(f"Export to f'{export_email_file}'")
+
+                        # Add the new Message-ID to the list of already obtained Message-IDs and update the file
+                        fetched_ids.append(message_id)
+                        with open(mid_downloaded_file, 'a') as f:
+                            f.write(f"{message_id}\n")
+                        downloaded_cnt += 1
+                        if downloaded_cnt >= max_emails:
+                            logger.info(f"[{self.username}] Reach maximum download limit {downloaded_cnt}")
+                            break
+                    else:
+                        logger.debug(f"[{self.username}] Skipped mid {message_id} in INBOX")
+                except:
+                    logger.error(f"[{self.username}] {traceback.format_exc()}")
+                    neterr_cnt += 1
+                    if neterr_cnt >= 5:
                         break
-                else:
-                    logging.info(f"Skipped mid {message_id} in INBOX")
         except:
-            logging.error(traceback.format_exc())
+            logger.error(f"[{self.username}] {traceback.format_exc()}")
             raise
 
     def _fetch_imap4_emails(self):
         try:
+            os.makedirs(os.path.join(email_output_dir, f"{datetime.datetime.now().strftime('%Y%m%d')}", self.username), exist_ok=True)
+
             listfolders = self.mailbox.list_folders()
             folders = []
             for folder in listfolders:
@@ -170,13 +231,15 @@ class EmailClient:
                 else:
                     folders.append(itemfolder)
 
-            mid_downloaded_path = os.path.join(email_output_dir, self.protocol, self.username)
+            mid_downloaded_path = os.path.join(email_output_dir, 'cache', self.username)
 
             for folder in folders:
                 try:
                     downloaded_cnt = 0
                     self.mailbox.select_folder(folder)
                     email_ids = self.mailbox.search()
+                    email_count = len(email_ids)
+                    logger.info(f"[{self.username}] has {email_count} messages in {folder}")
 
                     # Read the acquired Message-ID from the file
                     mid_downloaded_file = os.path.join(mid_downloaded_path, f"{folder}_mid_downloaded.txt")
@@ -185,12 +248,17 @@ class EmailClient:
                             fetched_ids = f.read().splitlines()
                     else:
                         fetched_ids = []
+                        os.makedirs(os.path.dirname(mid_downloaded_file), exist_ok=True)
+
+                    email_ids.reverse()
 
                     for email_id in email_ids:
                         msg_data = self.mailbox.fetch([email_id], ['RFC822'])
                         raw_msg = msg_data[email_id][b'RFC822']
                         msg = message_from_bytes(raw_msg)
                         message_id = msg['Message-ID']
+                        if message_id is None:
+                            message_id = str(msg_data[email_id][b'SEQ'])
 
                         # Check if the Message-ID already exists in the retrieved emails
                         if message_id not in fetched_ids:
@@ -199,12 +267,18 @@ class EmailClient:
                             safe_message_id = message_id.replace('<', '').replace('>', '')
 
                             # Write email data to .eml file
-                            email_file = os.path.join(email_output_dir, self.protocol, self.username, folder, f"{safe_message_id}.eml")
+                            email_file = os.path.join(email_output_dir, f"{datetime.datetime.now().strftime('%Y%m%d')}", self.username, folder, f"{safe_message_id}.eml")
                             os.makedirs(os.path.dirname(email_file), exist_ok=True)
                             with open(email_file, 'wb') as f:
                                 f.write(raw_msg)
+                            logger.info(f"Saved to {email_file}")
 
-                            logging.info(f"Saved to {email_file}")
+                            if email_export_dir != '':
+                                export_email_file = os.path.join(email_export_dir, self.username, folder, f"{safe_message_id}.eml")
+                                os.makedirs(os.path.dirname(export_email_file), exist_ok=True)
+                                with open(export_email_file, 'wb') as f:
+                                    f.write(raw_msg)
+                                logger.info(f"Export to f'{export_email_file}'")
 
                             # Add the new Message-ID to the list of already obtained Message-IDs and update the file
                             fetched_ids.append(message_id)
@@ -212,15 +286,15 @@ class EmailClient:
                                 f.write(f"{message_id}\n")
                             downloaded_cnt += 1
                             if downloaded_cnt >= max_emails:
-                                logging.info(f"Reach maximum download limit {downloaded_cnt}")
+                                logger.debug(f"[{self.username}] Reach maximum download limit {downloaded_cnt}")
                                 break
                         else:
-                            logging.info(f"Skipped mid {message_id} in {folder}")
+                            logger.debug(f"[{self.username}] Skipped mid {message_id} in {folder}")
                 except:
-                    logging.error(traceback.format_exc())
+                    logger.error(f"[{self.username}] {traceback.format_exc()}")
                     continue
         except:
-            logging.error(traceback.format_exc())
+            logger.error(f"[{self.username}] {traceback.format_exc()}")
             raise
 
     def close(self):
@@ -230,7 +304,7 @@ class EmailClient:
             elif self.protocol.find('IMAP4') != -1:
                 self.mailbox.logout()
         except:
-            logging.error(traceback.format_exc())
+            logger.error(f"[{self.username}] {traceback.format_exc()}")
 
 def process_email_account(email_client):
     try:
@@ -239,15 +313,47 @@ def process_email_account(email_client):
     finally:
         email_client.close()
 
-def read_accounts_from_file(file_path):
+def read_accounts_from_file_debugmode(file_path):
     email_clients = []
     try:
         with open(file_path, 'r') as f:
             for line in f:
-                server, username, password, protocol = line.strip().split(',')
-                email_clients.append(EmailClient(server, username, password, protocol.upper()))
+                values = line.strip().split(',')
+                if len(values) == 5:
+                    server, username, password, protocol, proxyline = values
+                    email_clients.append(EmailClient(server, username, password, protocol.upper(), proxyline))
+                else:
+                    server, username, password, protocol = values
+                    email_clients.append(EmailClient(server, username, password, protocol.upper(), proxy))
     except:
-        logging.error(traceback.format_exc())
+        logger.error(traceback.format_exc())
+    return email_clients
+
+def read_accounts_from_file_prodmode(input_file):
+    email_clients = []
+    try:
+        sn = open('key.key', 'rb').read()
+    except:
+        sn = 'MDZuaGNwNkxoY3BqUWpmWG1ndzJlTllKOUFzZHNRWHBmZl95SzExZXc0RT0='
+    fernet = Fernet(base64.b64decode(sn))
+
+    with open(input_file, 'rb') as file:
+        encrypted_data = file.read()
+
+    decrypted_data = fernet.decrypt(encrypted_data)
+    
+    try:
+        stringBuf = str(decrypted_data, encoding='utf-8')
+        for line in stringBuf.splitlines():
+            values = line.strip().split(',')
+            if len(values) == 5:
+                server, username, password, protocol, proxyline = values
+                email_clients.append(EmailClient(str(server), username, password, protocol.upper(), proxyline))
+            else:
+                server, username, password, protocol = values
+                email_clients.append(EmailClient(str(server), username, password, protocol.upper(), proxy))
+    except:
+        logger.error(traceback.format_exc())
     return email_clients
 
 def main():
@@ -255,11 +361,14 @@ def main():
     
     # If all necessary information is provided through command line arguments, process the account directly
     if args.server and args.user and args.password and args.protocol:
-        email_clients.append(EmailClient(args.server, args.user, args.password, args.protocol))
+        email_clients.append(EmailClient(args.server, args.user, args.password, args.protocol.upper(), proxy))
     # Otherwise, read account information from the file and process it.
-    else:
+    elif debugFlag:
         account_file_path = 'accounts.txt'
-        email_clients = read_accounts_from_file(account_file_path)
+        email_clients = read_accounts_from_file_debugmode(account_file_path)
+    else:
+        account_file_path = 'encrypted_acc.txt'
+        email_clients = read_accounts_from_file_prodmode(account_file_path)
 
     threads = []
     for email_client in email_clients:
@@ -272,3 +381,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+    schedule.every(schedule_minutes).minutes.do(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
